b665b94d5b6398511e3f79c025ea7ae7ec895d82
Delabeneta <crazayfrog@gmail.com>
2026-04-14 11:42:21 -0300

    fix: final

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'https://controlefinanceiro-theta.vercel.app',
-      'https://controle-financeiro-web-back.vercel.app',
+      'https://controle-financeiroback.vercel.app',
       'http://localhost:3000',
       'http://localhost:3001',
       /\.vercel\.app$/,
